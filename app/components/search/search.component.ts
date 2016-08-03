a17cf66f299a12f1b31d0d3ac58d83d95235f880
taljacobson <tal1989@gmail.com>
2016-08-03 18:25:27 +0300

    search spotify api

--- app/components/search/search.component.ts
+++ app/components/search/search.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { SpotifyService } from "../../serivces/spotify.service";
+import { HTTP_PROVIDERS } from "@angular/http";
 
 @Component({
     moduleId: module.id,
     selector: 'search',
-    templateUrl: 'search.component.html'
+    templateUrl: 'search.component.html',
+    providers: [ HTTP_PROVIDERS, SpotifyService ]
 })
-export class SearchComponent { }
+export class SearchComponent {
+  searchStr:string;
+  SearchRes: Result[];
+
+  constructor(private _SpotifyService: SpotifyService){
+    // this.searchStr = 'green day';
+
+  }
+  searchMusic(){
+    this._SpotifyService.searchMusic(this.searchStr)
+      .subscribe(res => {
+        this.SearchRes = res.artists.items;
+      })
+  }
+}
+
+class Result {
+  id: number;
+  name: string;
+  genre: any;
+}
