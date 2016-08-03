42b9df431eb64a4598c7209273f82df27b478405
taljacobson <tal1989@gmail.com>
2016-08-03 18:25:07 +0300

    router direct

--- app/components/navbartop/navbartop.component.ts
+++ app/components/navbartop/navbartop.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES} from "@angular/router";
 
 @Component({
     moduleId: module.id,
     selector: 'navbar-top',
-    templateUrl: 'navbartop.component.html'
+    templateUrl: 'navbartop.component.html',
+    directives: [ ROUTER_DIRECTIVES ]
 })
 export class NavBarTopComponent { }
